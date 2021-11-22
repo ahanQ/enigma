bea7af7cf1b3939da1b924901de703932db160f1
luliuquan <luliuquan@weatherone.com.cn>
2021-11-22 11:32:19 +0800

    封装起各个部件

--- src/main/java/enigma/Enigma.java
+++ src/main/java/enigma/Enigma.java
@@ -1,6 +1,6 @@
 package enigma;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * 恩格尼玛机
@@ -9,11 +9,171 @@ import java.util.List;
  * @date 2021/11/19 11:33
  */
 public class Enigma {
+    private static final Random RANDOM = new Random(17);
+    public final Reflector reflector = new Reflector();
+    public static final Rotor ROTOR_1 = new Rotor();
+    public static final Rotor ROTOR_2 = new Rotor();
+    public static final Rotor ROTOR_3 = new Rotor();
     public final List<Rotor> rotors;
-    public final Reflector reflector;
 
-    public Enigma(Reflector reflector, List<Rotor> rotors) {
-        this.reflector = reflector;
-        this.rotors = rotors;
+    public Enigma() {
+        // 初始化恩尼格玛机
+        // 默认第一个转子设置
+        {
+            final int[] front = ROTOR_1.front;
+            final int[] back = ROTOR_1.back;
+            int count = 0;
+            while (count < 26) {
+                int r = (RANDOM.nextInt(26) + count) % 26;
+                if (front[count] == -1 && back[r] == -1) {
+                    front[count] = r;
+                    back[r] = count;
+                    count++;
+                }
+            }
+        }
+        // 默认第二个转子设置
+        {
+            final int[] front = ROTOR_2.front;
+            final int[] back = ROTOR_2.back;
+            int count = 0;
+            while (count < 26) {
+                int r = (RANDOM.nextInt(26) + count) % 26;
+                if (front[count] == -1 && back[r] == -1) {
+                    front[count] = r;
+                    back[r] = count;
+                    count++;
+                }
+            }
+        }
+        // 默认第三个转子设置
+        {
+            final int[] front = ROTOR_3.front;
+            final int[] back = ROTOR_3.back;
+            int count = 0;
+            while (count < 26) {
+                int r = (RANDOM.nextInt(26) + count) % 26;
+                if (front[count] == -1 && back[r] == -1) {
+                    front[count] = r;
+                    back[r] = count;
+                    count++;
+                }
+            }
+        }
+        this.rotors = Arrays.asList(ROTOR_1, ROTOR_2, ROTOR_3);
     }
+
+    /**
+     * 输入一个字符，输出加密后的字符
+     *
+     * @param input 输入
+     * @return 输出
+     */
+    public char trigger(char input) {
+        int s = input - 97;
+        final List<Rotor> rotors = this.rotors;
+        // 正面进入转子
+        for (final Rotor rotor : rotors) {
+            s = rotorInput(rotor, s);
+        }
+        // 通过反射板
+        s = triggerReflector(this.reflector, s);
+        // 背面进入转子
+        for (int i = rotors.size() - 1; i >= 0; i--) {
+            final Rotor rotor = rotors.get(i);
+            s = rotorOutput(rotor, s);
+        }
+        // 转子转动
+        boolean isNext;
+        int index = 0;
+        do {
+            isNext = false;
+            final Rotor rotor = rotors.get(index++);
+            rotor.position++;
+            while (rotor.position >= 26) {
+                rotor.position -= 26;
+                isNext = true;
+            }
+        } while (isNext && index < rotors.size());
+        // 返回结果
+        return (char) (s + 97);
+    }
+
+    /**
+     * 反射板的映射
+     *
+     * @param reflector 反射板
+     * @param input     输入
+     * @return 输出
+     */
+    private int triggerReflector(Reflector reflector, int input) {
+        return reflector.lines.get(input);
+    }
+
+    /**
+     * 入口端进入转子
+     *
+     * @param rotor 转子
+     * @param input 输入
+     * @return 输出
+     */
+    private int rotorInput(Rotor rotor, int input) {
+        return (rotor.front[(input + rotor.position) % 26] + 26 - rotor.position) % 26;
+    }
+
+    /**
+     * 返回端进入转子
+     *
+     * @param rotor 转子
+     * @param input 输入
+     * @return 输出
+     */
+    private int rotorOutput(Rotor rotor, int input) {
+        return (rotor.back[(input + rotor.position) % 26] + 26 - rotor.position) % 26;
+    }
+
+    /**
+     * 转子
+     */
+    public static class Rotor {
+        public final int[] front = new int[]{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+        public final int[] back = new int[]{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+        public int position;
+    }
+
+    public static class Reflector {
+
+        public final Map<Integer, Integer> lines = new LinkedHashMap<>();
+
+        public Reflector() {
+            // 默认反射反设置
+            lines.put(0, 13);
+            lines.put(1, 14);
+            lines.put(2, 15);
+            lines.put(3, 16);
+            lines.put(4, 17);
+            lines.put(5, 18);
+            lines.put(6, 19);
+            lines.put(7, 20);
+            lines.put(8, 21);
+            lines.put(9, 22);
+            lines.put(10, 23);
+            lines.put(11, 24);
+            lines.put(12, 25);
+            lines.put(13, 0);
+            lines.put(14, 1);
+            lines.put(15, 2);
+            lines.put(16, 3);
+            lines.put(17, 4);
+            lines.put(18, 5);
+            lines.put(19, 6);
+            lines.put(20, 7);
+            lines.put(21, 8);
+            lines.put(22, 9);
+            lines.put(23, 10);
+            lines.put(24, 11);
+            lines.put(25, 12);
+        }
+    }
+
 }
--- src/main/java/enigma/Main.java
+++ src/main/java/enigma/Main.java
@@ -1,8 +1,5 @@
 package enigma;
 
-import java.util.Arrays;
-import java.util.Random;
-
 /**
  * 运行主程序
  *
@@ -10,81 +7,37 @@ import java.util.Random;
  * @date 2021/11/19 11:33
  */
 public class Main {
-    private static Random random = new Random(17);
-    private static final Enigma enigma;
-
-    static {
-        // 初始化恩尼格玛机
-        final Reflector reflector = new Reflector();
-        final Rotor rotor1 = new Rotor();
-        // 默认第一个转子设置
-        {
-            final int[] front = rotor1.front;
-            final int[] back = rotor1.back;
-            int count = 0;
-            while (count < 26) {
-                int r = (random.nextInt(26) + count) % 26;
-                if (front[count] == -1 && back[r] == -1) {
-                    front[count] = r;
-                    back[r] = count;
-                    count++;
-                }
-            }
-        }
-        final Rotor rotor2 = new Rotor();
-        // 默认第二个转子设置
-        {
-            final int[] front = rotor2.front;
-            final int[] back = rotor2.back;
-            int count = 0;
-            while (count < 26) {
-                int r = (random.nextInt(26) + count) % 26;
-                if (front[count] == -1 && back[r] == -1) {
-                    front[count] = r;
-                    back[r] = count;
-                    count++;
-                }
-            }
-        }
-        final Rotor rotor3 = new Rotor();
-        // 默认第二个转子设置
-        {
-            final int[] front = rotor3.front;
-            final int[] back = rotor3.back;
-            int count = 0;
-            while (count < 26) {
-                int r = (random.nextInt(26) + count) % 26;
-                if (front[count] == -1 && back[r] == -1) {
-                    front[count] = r;
-                    back[r] = count;
-                    count++;
-                }
-            }
-        }
-        enigma = new Enigma(reflector, Arrays.asList(rotor1, rotor2, rotor3));
-    }
+    private static final Enigma ENIGMA = new Enigma();
 
     public static void main(String[] args) {
         final char[] result = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz".toCharArray();
-        for (char c : result) {
-            System.out.print(c);
+        print(result);
+        ENIGMA.rotors.get(0).position = 1;
+        ENIGMA.rotors.get(1).position = 0;
+        ENIGMA.rotors.get(2).position = 0;
+        encryptionAndPrint(ENIGMA, result);
+        ENIGMA.rotors.get(0).position = 1;
+        ENIGMA.rotors.get(1).position = 0;
+        ENIGMA.rotors.get(2).position = 0;
+        encryptionAndPrint(ENIGMA, result);
+    }
+
+    private static void print(char[] chars) {
+        for (char aChar : chars) {
+            System.out.print(aChar);
         }
         System.out.println();
-        enigma.rotors.get(0).position = 1;
-        enigma.rotors.get(1).position = 0;
-        enigma.rotors.get(2).position = 0;
-        Run.encryption(enigma, result);
-        for (char c : result) {
-            System.out.print(c);
+    }
+
+
+    private static void encryptionAndPrint(Enigma enigma, char[] chars) {
+        encryption(enigma, chars);
+        print(chars);
+    }
+
+    private static void encryption(Enigma enigma, char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = enigma.trigger(chars[i]);
         }
-        System.out.println();
-        enigma.rotors.get(0).position = 1;
-        enigma.rotors.get(1).position = 0;
-        enigma.rotors.get(2).position = 0;
-        Run.encryption(enigma, result);
-        for (char c : result) {
-            System.out.print(c);
-        }
-        System.out.println();
     }
 }
